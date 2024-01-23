a51079932c24926c923eda3be93976ee5f07bf18
Juanzan <38819946+juanzandev@users.noreply.github.com>
2024-01-23 16:54:18 -0600

    Create extensions.py

---
+++ extensions.py
@@ -0,0 +1,28 @@
+# Implement a program that prompts the user for the name of a file and then outputs that file’s media type if the file’s name ends,
+# case-insensitively, in any of these suffixes:
+
+# .gif
+# .jpg
+# .jpeg
+# .png
+# .pdf
+# .txt
+# .zip
+# Else, return application/octet-stream
+
+
+# STEPS TO IMPLEMENT THE PROGRAM:
+# 1. prompt the user for the name of a file
+# 2. convert the file name to lowercase and get rid of any leading or trailing whitespace
+# 3. check if the file name ends with the suffixes
+# 4. if the file name ends with the suffixes, print the media type
+# 5. if the file name does not end with the suffixes, print the default media type
+# 6. remember to check the reference for string methods: https://docs.python.org/3/library/stdtypes.html#string-methods
+# 7. remember to check the reference for if statement: https://docs.python.org/3/reference/compound_stmts.html#if
+# 8. remember to check the reference for input function: https://docs.python.org/3/library/functions.html#input
+# 9. remember to check the reference for print function: https://docs.python.org/3/library/functions.html#print
+# HINTS:
+# 1. you won't need to create multiple fuctions for this program
+# 2. you won't need to use any modules for this program
+# 3. you won't need to use any loops for this program
+# 4. you won't need to use any conditional expressions for this program such as ==, !=, <, >, <=, >=.
