2af22cc2b543b9680c2ee87f9763aa0836e78a8b
Juanzan <38819946+juanzandev@users.noreply.github.com>
2024-01-23 16:09:26 -0600

    Update bank.py

--- bank.py
+++ bank.py
@@ -1,1 +1,23 @@
+# OBJECTIVE:
+# Implement a program that prompts the user for a greeting.
+# If the greeting starts with “hello”, output $0.
+# If the greeting starts with an “h” (but not “hello”), output $20.
+# Otherwise, output $100. Ignore any leading whitespace in the user’s greeting,
+# and treat the user’s greeting case-insensitively.
 
+# Reference link: https://www.w3schools.com/python/python_strings.asp
+# ADDITIONAL NOTES:
+# the . is used to call a method on a string
+# a method is a function that belongs to a class
+
+# STEPS TO SOLVE THIS PROBLEM:
+# 1. ask the user a question
+# 2. store the answer in a variable
+# 3. format the string to remove leading whitespace
+# 4. format the string to lowercase
+# 5. check if the answer starts with "hello"
+# 6. if the answer starts with "hello", print "$0"
+# 7. check if the answer starts with "h"
+# 8. if the answer starts with "h", print "$20"
+# 9. otherwise, print "$100"
+# remember to call the main function at the end of the program
