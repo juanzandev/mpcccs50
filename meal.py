3ca69fbe322b1eb3b2c916c1fa69a92e70edd28e
Juanzan <38819946+juanzandev@users.noreply.github.com>
2024-01-23 17:41:02 -0600

    Create meal.py

---
+++ meal.py
@@ -0,0 +1,29 @@
+def main():
+    # FIRST PART
+    # initialize a variable called time
+    # ask the user a question
+    # store the answer in a variable
+    # call the convert function and pass in the time variable. store the return value in a variable
+
+    # THIRD PART
+    # use an if statement to check for desired conditions
+    # if hours is between 7.00 and 8.00, print "breakfast time"
+    # if hours is between 12.00 and 13.00, print "lunch time"
+    # if hours is between 18.00 and 19.00, print "dinner time"
+    # otherwise, print "not meal time"
+
+def convert(time):
+    #SECOND PART
+    # convert the time to a float. The float should be 7.5 if the time is 7:30
+    # split the time into hours and minutes. reference for string methods: https://docs.python.org/3/library/stdtypes.html#string-methods
+    # convert the hours and minutes into floats
+    # --------------------------------------------------------------------------------------------------------------
+    # EXTRA CREDIT:
+    # if minutes is higher than 59, the user should be told that the time is invalid and the program ends there.
+    # --------------------------------------------------------------------------------------------------------------
+    # return the sum of the hours and minutes divided by 60. (only divide minutes by 60, not the entire thing).
+    
+
+
+if __name__ == "__main__":
+    main()
