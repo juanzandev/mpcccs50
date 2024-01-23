424b083ce4815d09b185ebf206ea7747a82f8c28
Juanzan <38819946+juanzandev@users.noreply.github.com>
2024-01-23 15:59:27 -0600

    Create deep.py

---
+++ deep.py
@@ -0,0 +1,9 @@
+# deep.py instructions
+#explain the code below
+# 1. ask the user a question
+# 2. store the answer in a variable
+# 3. check if the answer is correct
+# 4. if the answer is correct, print "Yes"
+# remember to use == to compare strings
+# remember to use elif to check multiple conditions
+# 5. if the answer is incorrect, print "No"
