18366580a864a4d88333c9ba6bc30fb4877440bc
Juanzan <38819946+juanzandev@users.noreply.github.com>
2024-01-29 18:18:16 -0600

    Create nutrition.py

---
+++ pset2/nutrition.py
@@ -0,0 +1,5 @@
+#Start by defining a dictionary where the keys are the names of the fruits and the values are the corresponding calories.
+#Prompt the user for a fruit name. Store the user's input in a variable. Convert the user's input to lowercase to make the comparison case-insensitive.
+#Iterate over the keys in the dictionary using a loop. For each key (fruit name), convert it to lowercase.
+#Inside the loop, check if the lowercase version of the current key matches the user's input. If they match, print the value (calories) associated with the current key. Convert the value to a string before printing. Then, break the loop.
+#Finally, make sure to call the main part of your program only when the script is run directly (not imported as a module) by checking a certain condition. If true, call the main part of your program.
