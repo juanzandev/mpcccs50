fb2db0f021d1477a170a980d89a47c1b43434b27
Juanzan <38819946+juanzandev@users.noreply.github.com>
2024-02-17 21:37:53 -0600

    Create emojize.py

---
+++ pset4/emojize.py
@@ -0,0 +1,14 @@
+'''
+This is a very short problem.
+Learn how to import a library; in this case, the name is emoji. Go to the hints section of the problem and install the library using the instructions they give you.
+Create a variable to store the user's input. https://pypi.org/project/emoji/
+Then print "Output: " + the variable, but it is a bit trickier than that.
+To use a library's function, you must precede the method you want to use with the library's name.
+I. e: if there is a library called math and you want to use the addition function, you would write it like this
+    math.addition()
+'''
+import ?
+def main():
+  # input_variable = input()
+  # print(?)
+  # Hint: be careful when you read what the function emojize does and how to use it. It may not be as obvious as it sounds. You could totally take a different approach to this problem, and if you do, please let me know
